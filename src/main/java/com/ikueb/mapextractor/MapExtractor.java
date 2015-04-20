a4a97494e5d3236fa9ed7908e930df2d54720e34
h-j-k <han.junkwang@gmail.com>
2015-04-21 00:38:13 +0800

    tweaked Javadocs, APIs and underlying implementations.

--- src/main/java/com/ikueb/mapextractor/MapExtractor.java
+++ src/main/java/com/ikueb/mapextractor/MapExtractor.java
@@ -17,6 +17,28 @@ import java.util.stream.Collector.Characteristics;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * An utility class providing:
+ * <ul>
+ * <li>A set of helper methods to easily convert {@link Stream}s of {@link CharSequence}
+ * s to either {@link Properties} or {@link Map} instances.
+ * <ul>
+ * <li>These methods operate on streams and reproduces the parsing logic in
+ * {@link Properties#load(java.io.Reader)} as closely as possible. The only differences
+ * are no support for multi-line values and the default key delimiter can only be
+ * escaped with {@code "\"} once.</li>
+ * </ul>
+ * </li>
+ * <li>A set of collectors to be used in {@link Stream#collect(Collector)} as terminal
+ * operations.
+ * <ul>
+ * <li>These methods are meant to be drop-in replacements for the standard JDK
+ * {@code Collectors.toMap()} methods, but with a {@code regex} prefixed argument to
+ * split each stream element into key-value pairings.</li>
+ * </ul>
+ * </li>
+ * </ul>
+ */
 public final class MapExtractor {
 
     /**
@@ -28,7 +50,7 @@ public final class MapExtractor {
      * Regular expression for matching key delimiters (taking care to check if they are
      * escaped).
      */
-    private static final String DELIMITER = "(?<!\\\\)[:=]";
+    private static final String REGEX_DELIMITER = "(?<!\\\\)[:=]";
 
     private static final String JOIN_DELIMITER = ", ";
 
@@ -38,7 +60,8 @@ public final class MapExtractor {
 
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
-     * instance, except that multi-line values are not supported.
+     * instance, except that multi-line values are not supported and the default key
+     * delimiter can only be escaped with {@code "\"} once.
      *
      * @param entries lines to process
      * @return a {@link Properties} instance based on {@code entries}
@@ -46,7 +69,7 @@ public final class MapExtractor {
      */
     public static Properties asProperties(final Stream<? extends CharSequence> entries) {
         final Properties props = new Properties();
-        props.putAll(skipComments(entries).collect(toMap(DELIMITER, toKey(), toValue(),
+        props.putAll(skipComments(entries).collect(toMap(REGEX_DELIMITER, toKey(), toValue(),
                 (a, b) -> { return b; })));
         return props;
     }
@@ -54,20 +77,22 @@ public final class MapExtractor {
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
      * instance, except that values for the same key are put into a {@link List} and
-     * multi-line values are not supported.
+     * multi-line values are not supported and the default key delimiter can only be
+     * escaped with {@code "\"} once.
      *
      * @param entries lines to process
      * @return a {@link Map}
      */
     public static Map<String, List<String>> groupingBy(
             final Stream<? extends CharSequence> entries) {
-        return skipComments(entries).collect(groupingBy(DELIMITER, toKey(), toValue()));
+        return skipComments(entries).collect(groupingBy(REGEX_DELIMITER, toKey(), toValue()));
     }
 
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
-     * instance, except that duplicate keys results in an {@link IllegalStateException}
-     * and multi-line values are not supported.
+     * instance, except that duplicate keys results in an {@link IllegalStateException},
+     * multi-line values are not supported and the default key delimiter can only be
+     * escaped with {@code "\"} once.
      *
      * @param entries lines to process
      * @return a {@link Map}
@@ -75,13 +100,14 @@ public final class MapExtractor {
      */
     public static Map<String, String> simpleMap(
             final Stream<? extends CharSequence> entries) {
-        return skipComments(entries).collect(toMap(DELIMITER, toKey(), toValue()));
+        return skipComments(entries).collect(toMap(REGEX_DELIMITER, toKey(), toValue()));
     }
 
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
-     * instance, except that values for the same key are joined and multi-line values
-     * are not supported.
+     * instance, except that values for the same key are joined, multi-line values are
+     * not supported and the default key delimiter can only be escaped with {@code "\"}
+     * once.
      *
      * @param entries lines to process
      * @return a {@link Map}
@@ -94,7 +120,8 @@ public final class MapExtractor {
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
      * instance, except that values for the same key are joined using
-     * {@code joinDelimiter} and multi-line values are not supported.
+     * {@code joinDelimiter}, multi-line values are not supported and the default key
+     * delimiter can only be escaped with {@code "\"} once.
      *
      * @param entries lines to process
      * @param joinDelimiter the join delimiter to use
@@ -102,73 +129,72 @@ public final class MapExtractor {
      */
     public static Map<String, String> simpleMapAndJoin(
             final Stream<? extends CharSequence> entries, final String joinDelimiter) {
-        return skipComments(entries).collect(toMapAndJoin(DELIMITER, joinDelimiter));
+        return skipComments(entries).collect(toMapAndJoin(REGEX_DELIMITER, joinDelimiter));
     }
 
 
     /**
-     * With the exception of the first argument {@code delimiter} for parsing each
-     * stream element, this method is meant to be used in a similar way as the
-     * equivalent in the {@link Collectors} class, except that the third argument
-     * {@code valueMapper} exists to do the conversion of map values as well, instead of
-     * using the stream elements.
+     * With the exception of the first argument {@code regex} for splitting each stream
+     * element, this method is meant to be used in a similar way as the equivalent in
+     * the {@link Collectors} class, except that the third argument {@code valueMapper}
+     * exists to do the conversion of map values as well, instead of using the stream
+     * elements.
      * <p>
      * Implementation note: the aggregation on values is done by mapping each value as a
      * single-element {@link List}, and then calling
      * {@link List#addAll(java.util.Collection)} as the merge function to
      * {@link MapExtractor#toMap(String, Function, Function, BinaryOperator)}.
      *
-     * @param delimiter the delimiter to use
+     * @param regex the delimiter to use
      * @param keyMapper
      * @param valueMapper
      * @return a {@link Map}
      * @see Collectors#groupingBy(Function)
      */
     public static <K, V> Collector<CharSequence, ?, Map<K, List<V>>> groupingBy(
-            final String delimiter, final Function<? super CharSequence, K> keyMapper,
+            final String regex, final Function<? super CharSequence, K> keyMapper,
             final Function<? super CharSequence, V> valueMapper) {
-        return toMap(delimiter, keyMapper,
-                value -> new ArrayList<>(Arrays.asList(valueMapper.apply(value))),
-                (a, b) -> { a.addAll(b); return a; });
+        return toMap(regex, keyMapper, enlist(valueMapper), joinList());
     }
 
     /**
      * Handles the stream like the line-oriented format for loading a {@link Properties}
      * instance, except that values for the same key are joined using
-     * {@code joinDelimiter} and multi-line values are not supported.
+     * {@code joinDelimiter}, multi-line values are not supported and the default key
+     * delimiter can only be escaped with {@code "\"} once.
      *
-     * @param delimiter the delimiter to use
+     * @param regex the delimiter to use
      * @param joinDelimiter the join delimiter to use
      * @return a {@link Collector}
      */
     public static Collector<CharSequence, ?, Map<String, String>> toMapAndJoin(
-            final String delimiter, final String joinDelimiter) {
-        return toMap(delimiter, toKey(), toValue(), join(joinDelimiter));
+            final String regex, final String joinDelimiter) {
+        return toMap(regex, toKey(), toValue(), join(joinDelimiter));
     }
 
     /**
-     * With the exception of the first argument {@code delimiter} for parsing each
-     * stream element, this method is meant to be used in the same way as the equivalent
-     * in the {@link Collectors} class.
+     * With the exception of the first argument {@code regex} for splitting each stream
+     * element, this method is meant to be used in the same way as the equivalent in the
+     * {@link Collectors} class.
      *
-     * @param delimiter the delimiter to use
+     * @param regex the delimiter to use
      * @param keyMapper
      * @param valueMapper
      * @return a {@link Map}
      * @see Collectors#toMap(Function, Function)
      */
     public static <K, V> Collector<CharSequence, ?, Map<K, V>> toMap(
-            final String delimiter, final Function<? super CharSequence, K> keyMapper,
+            final String regex, final Function<? super CharSequence, K> keyMapper,
             final Function<? super CharSequence, V> valueMapper) {
-        return toMap(delimiter, keyMapper, valueMapper, duplicateKeyMergeThrower());
+        return toMap(regex, keyMapper, valueMapper, duplicateKeyMergeThrower());
     }
 
     /**
-     * With the exception of the first argument {@code delimiter} for parsing each
-     * stream element, this method is meant to be used in the same way as the equivalent
-     * in the {@link Collectors} class.
+     * With the exception of the first argument {@code regex} for splitting each stream
+     * element, this method is meant to be used in the same way as the equivalent in the
+     * {@link Collectors} class.
      *
-     * @param delimiter the delimiter to use
+     * @param regex the delimiter to use
      * @param keyMapper
      * @param valueMapper
      * @param mergeFunction
@@ -176,18 +202,18 @@ public final class MapExtractor {
      * @see Collectors#toMap(Function, Function, BinaryOperator)
      */
     public static <K, V> Collector<CharSequence, ?, Map<K, V>> toMap(
-            final String delimiter, final Function<? super CharSequence, K> keyMapper,
+            final String regex, final Function<? super CharSequence, K> keyMapper,
             final Function<? super CharSequence, V> valueMapper,
             final BinaryOperator<V> mergeFunction) {
-        return toMap(delimiter, keyMapper, valueMapper, mergeFunction, HashMap::new);
+        return toMap(regex, keyMapper, valueMapper, mergeFunction, HashMap::new);
     }
 
     /**
-     * With the exception of the first argument {@code delimiter} for parsing each
-     * stream element, this method is meant to be used in the same way as the equivalent
-     * in the {@link Collectors} class.
+     * With the exception of the first argument {@code regex} for splitting each stream
+     * element, this method is meant to be used in the same way as the equivalent in the
+     * {@link Collectors} class.
      *
-     * @param delimiter the delimiter to use
+     * @param regex the delimiter to use
      * @param keyMapper
      * @param valueMapper
      * @param mergeFunction
@@ -196,15 +222,14 @@ public final class MapExtractor {
      * @see Collectors#toMap(Function, Function, BinaryOperator, Supplier)
      */
     public static <K, V, M extends Map<K, V>> Collector<CharSequence, ?, M> toMap(
-            final String delimiter, final Function<? super CharSequence, K> keyMapper,
+            final String regex, final Function<? super CharSequence, K> keyMapper,
             final Function<? super CharSequence, V> valueMapper,
             final BinaryOperator<V> mergeFunction, final Supplier<M> mapSupplier) {
-        Stream.of(delimiter, keyMapper, valueMapper, mergeFunction, mapSupplier)
+        Stream.of(regex, keyMapper, valueMapper, mergeFunction, mapSupplier)
                 .forEach(Objects::requireNonNull);
         return Collector.of(mapSupplier,
-                (map, entry) -> {
-                    CharSequence[] pair = toPair(entry, delimiter);
-                    map.merge(keyMapper.apply(pair[0]), valueMapper.apply(pair[1]),
+                (m, i) -> { CharSequence[] pair = splitWith(regex).apply(i);
+                    m.merge(keyMapper.apply(pair[0]), valueMapper.apply(pair[1]),
                             mergeFunction); },
                 (a, b) -> { b.entrySet().stream().forEach(
                     entry -> a.merge(entry.getKey(), entry.getValue(), mergeFunction));
@@ -219,8 +244,25 @@ public final class MapExtractor {
      * @param stream the stream to filter
      * @return a {@link Predicate} to skip comment lines
      */
-    private static <T extends CharSequence> Stream<T> skipComments(Stream<T> stream) {
+    private static <T extends CharSequence> Stream<T> skipComments(
+            final Stream<T> stream) {
         return stream.filter(v -> !COMMENTS.matcher(v).matches());
+    }
+
+    /**
+     * Splits {@code entry} with {@code regex} into a pair of sub-{@link String}s using
+     * {@link String#split(String, int)} (the second argument being {@code 2}).
+     *
+     * @param regex the delimiter to use
+     * @return a pair of {@link String}s, the second element will be an empty
+     *         {@link String} ({@code ""}) if there is no second sub-{@link String} from
+     *         the split
+     */
+    private static Function<? super CharSequence, CharSequence[]> splitWith(
+            final String regex) {
+        return v -> { String[] result = v.toString().split(regex, 2);
+            return result.length == 2 ? result : new String[] { result[0], "" };
+        };
     }
 
     /**
@@ -240,19 +282,19 @@ public final class MapExtractor {
     }
 
     /**
-     * Splits {@code entry} with {@code delimiter} into a pair of sub-{@link String}s
-     * using {@link String#split(String, int)} (the second argument being {@code 2}).
-     *
-     * @param entry the input to split
-     * @param delimiter the delimiter to use
-     * @return a pair of {@link String}s, the second element will be an empty
-     *         {@link String} ({@code ""}) if there is no second sub-{@link String} from
-     *         the split
+     * @param mapper the mapper to apply first
+     * @return a {@link Function} that wraps an element in an {@link ArrayList}
      */
-    private static CharSequence[] toPair(final CharSequence entry,
-            final String delimiter) {
-        final String[] result = entry.toString().split(delimiter, 2);
-        return result.length == 2 ? result : new String[] { result[0], "" };
+    private static <T, U> Function<? super T, List<U>> enlist(
+            final Function<? super T, U> mapper) {
+        return mapper.andThen(v -> new ArrayList<>(Arrays.asList(v)));
+    }
+
+    /**
+     * @return the first {@link List} appended with the contents of the second one
+     */
+    private static <T> BinaryOperator<List<T>> joinList() {
+        return (a, b) -> { a.addAll(b); return a; };
     }
 
     /**
@@ -270,7 +312,7 @@ public final class MapExtractor {
      * @return a {@link BinaryOperator} that always throw an
      *         {@link IllegalStateException}
      */
-    private static <V> BinaryOperator<V> duplicateKeyMergeThrower() {
+    private static <T> BinaryOperator<T> duplicateKeyMergeThrower() {
         return (a, b) -> {
             throw new IllegalStateException(String.format(
                     "Duplicate key for values \"%s\" and \"%s\".", a, b));
