e14c99ce7c10e2ccbda1c9e864d130ca28c4ccb7
h-j-k <han.junkwang@gmail.com>
2015-08-29 16:18:52 +0800

    added parallel mapping unit test.

--- src/test/java/com/ikueb/mapextractor/MapExtractorTest.java
+++ src/test/java/com/ikueb/mapextractor/MapExtractorTest.java
@@ -30,7 +30,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -45,7 +48,7 @@ public class MapExtractorTest {
             "c : d ", " c : e", "c : f", "g\\=h=i", "j\\:k:l", "xyz", "");
     
     private static final List<String> COMMENTS = Arrays.asList(
-            " ! this is a comment", " # this is a comment");
+            "! this is a comment", " # this is a comment");
 
     private static final Supplier<Stream<String>> COMBINED = 
             () -> Stream.of(RECORDS, COMMENTS).map(Collection::stream)
@@ -56,7 +59,7 @@ public class MapExtractorTest {
     
     private static final Map<String, String> EMPTY_KEY_VALUE = toMap("", "");
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testAsProperties() throws IOException {
         final Properties props = new Properties();
         props.load(new StringReader(COMBINED.get()
@@ -65,7 +68,7 @@ public class MapExtractorTest {
                 equalTo(props));
     }
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testGroupingBy() {
         assertThat(MapExtractor.groupingBy(COMBINED.get()),
                 equalTo(toMap(asList("a", "c", "g=h", "j:k", "xyz"),
@@ -73,7 +76,7 @@ public class MapExtractorTest {
                                 asList("i"), asList("l"), asList("")))));
     }
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testSimpleMap() {
         assertThat(MapExtractor.simpleMap(Stream.of("key=value")),
                 equalTo(toMap("key", "value")));
@@ -84,12 +87,12 @@ public class MapExtractorTest {
         MapExtractor.simpleMap(Stream.of("a :b1", " a=b2"));
     }
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testSimpleMapAndJoin() {
         assertThat(MapExtractor.simpleMapAndJoin(COMBINED.get()), equalTo(EXPECTED));
     }
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testKeyAndValueMapping() {
         assertThat(Stream.of("key1=value1", "key2=value2")
                 .collect(MapExtractor.toMap("=",
@@ -98,7 +101,7 @@ public class MapExtractorTest {
                 equalTo(toMap("1YEK", "1eulav", "2YEK", "2eulav")));
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testEmptyMapping() {
         Supplier<Stream<String>> testValues = () -> Stream.of("", " ", "[ ]*", "[ ]+");
         testValues.get()
@@ -113,21 +116,42 @@ public class MapExtractorTest {
     }
     
     @Test
+    public void testParallelMapping() {
+        int n = 3000;
+        String join = ", ";
+        Stream<String> parallelCopies = IntStream.range(0, n).mapToObj(v -> COMBINED.get())
+                                            .reduce(Stream::concat).get().parallel();
+        Pattern comments = Pattern.compile("^\\s*([#!]|$)");
+        Function<? super CharSequence, String> toKey = k -> 
+            k.toString().trim().replace("\\", "");
+        Function<? super CharSequence, String> toValue = v -> 
+            v.toString().replaceFirst("^\\s+", "");
+        BinaryOperator<String> merger = (a, b) -> 
+            (a.isEmpty() && b.isEmpty()) || a.equals(b) ? a : String.join(join, a, b);
+        Map<String, String> expected = new HashMap<>(EXPECTED);
+        expected.computeIfPresent("c", (k, v) -> Collections.nCopies(n, v).stream()
+                .collect(Collectors.joining(join)));
+        assertThat(parallelCopies.filter(s -> !comments.matcher(s).find()).collect(
+                    MapExtractor.toMap("(?<!\\\\)[:=]", toKey, toValue, merger)),
+                equalTo(expected));
+    }
+    
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testCommaParsing() {
         testReadyParsing(",", () -> MapExtractor.withComma());
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testSemicolonParsing() {
         testReadyParsing(";", () -> MapExtractor.withSemicolon());
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testTabParsing() {
         testReadyParsing("\t", () -> MapExtractor.withTab());
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testNewlineParsing() {
         testReadyParsing(System.lineSeparator(), () -> MapExtractor.withNewline());
     }
@@ -142,19 +166,19 @@ public class MapExtractorTest {
         assertThat(parser.parse(rs + " " + rs),  equalTo(EMPTY_KEY_VALUE));
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testParsingWithRSFS() {
         assertThat(MapExtractor.with("#", "!").parse("a#a!", "a!b#a!c"), 
                 equalTo(toMap("a", "bc")));
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testParsingWithRSFSNullOFS() {
         assertThat(MapExtractor.with("!", "#", null).parse("a#b!a#c"), 
                 equalTo(toMap("a", "c")));
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testCustomParsing() {
         assertThat(MapExtractor.with("\\|", "!", String::toLowerCase, 
                     Integer::parseInt, (a, b) -> a + b).parse("A!1|a!2"),
@@ -162,7 +186,7 @@ public class MapExtractorTest {
                         Collections.singletonList(Integer.valueOf(3)))));
     }
 
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testValueMerging() {
         assertThat(Stream.of("key1=1,2", "key2=3,4", "key1=5,6").collect(
                 MapExtractor.toMap("=", Object::toString,
@@ -171,7 +195,7 @@ public class MapExtractorTest {
                         (a, b) -> a + b)).get("key1"), equalTo(Integer.valueOf(14)));
     }
     
-    @Test
+    @Test(threadPoolSize = 2, invocationCount = 2)
     public void testEmptyParsing() {
         assertThat(MapExtractor.with("", "").parse(""), equalTo(emptyMap()));
         assertThat(MapExtractor.with("", " ").parse(""), equalTo(emptyMap()));
