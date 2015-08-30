bc1f273b64171684a3e093a0a84b2d55357c6d55
h-j-k <han.junkwang@gmail.com>
2015-08-30 17:25:05 +0800

    updated scripts for Travis CI build to generate Javadocs.

--- .travis-scripts/javadoc.sh
+++ .travis-scripts/javadoc.sh
@@ -8,14 +8,17 @@ jdb=gh-pages
 websrc=$TRAVIS_BUILD_DIR/target/site/apidocs
 webtgt=$jdb
 mvn javadoc:javadoc
+cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-lgm "Cloning..."
-cd $HOME
-git clone --quiet --branch=$jdb https://${gh_token}@github.com/$myid/$myproj $webtgt > /dev/null
-pushd $webtgt
-lgm "Copying..."; rm -rf apidocs && cp -Rf $websrc .
-lgm "Adding..."; git add -f . > /dev/null
-lgm "Committing..."; git commit -m "Published Javadoc: build $TRAVIS_BUILD_NUMBER" > /dev/null
-lgm "Pushing..."; git push -fq origin $jdb > /dev/null
+lgm "Cloning..."; 
+	git clone -q -b $jdb https://${gh_token}@github.com/$myid/$myproj $webtgt > /dev/null
+lgm "Copying..."; 
+	pushd $webtgt; rm -rf apidocs && cp -Rf $websrc .
+lgm "Adding..."; 
+	git add -f . > /dev/null
+lgm "Committing..."; 
+	git commit -m "Published Javadoc: build $TRAVIS_BUILD_NUMBER" > /dev/null
+lgm "Pushing..."; 
+	git push -fq origin $jdb > /dev/null
 lgm "Done."
