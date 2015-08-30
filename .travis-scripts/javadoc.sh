591f008fb2a1cbd24980e85595953e3f28a8f8c8
h-j-k <han.junkwang@gmail.com>
2015-08-30 14:44:54 +0800

    updated scripts for Travis CI build to generate Javadocs.

--- .travis-scripts/javadoc.sh
+++ .travis-scripts/javadoc.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+lgm() { date "+%c\t$1\n"; }
 [[ "$TRAVIS_PULL_REQUEST" != false || "$TRAVIS_BRANCH" != master ]] \
-	&& exit || echo Generating Javadocs...
+	&& exit || lgm "Generating Javadocs..."
+myid=h-j-k
+myproj=mapextractor
+jdb=gh-pages
+websrc=$HOME/target/site/apidocs
+webtgt=$jdb
 mvn javadoc:javadoc
-ls -l target/site/apidocs
+ls -l $websrc; exit
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+lgm "Cloning..."
+git clone --quiet --branch=$jdb https://${GH_TOKEN}@github.com/$myid/$myproj $webtgt > /dev/null
+pushd $webtgt
+lgm "Copying..." && rm -rf apidocs && cp -Rf $websrc .
+lgm "Adding..." && git add -f . > /dev/null
+lgm "Committing..." && git commit -m "Published Javadoc: build $TRAVIS_BUILD_NUMBER" > /dev/null
+lgm "Pushing..." && git push -fq origin $jdb > /dev/null
+lgm "Done."
