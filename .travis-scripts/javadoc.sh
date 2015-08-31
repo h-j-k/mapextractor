b8980d4f7e8f52c988f2ac8fc235c61bdf3da375
h-j-k <han.junkwang@gmail.com>
2015-08-31 22:40:43 +0800

    testing Javadoc update.

--- .travis-scripts/javadoc.sh
+++ .travis-scripts/javadoc.sh
@@ -6,19 +6,23 @@ myid=${TRAVIS_REPO_SLUG%/*}
 myproj=${TRAVIS_REPO_SLUG##*/}
 jdb=gh-pages
 websrc=$TRAVIS_BUILD_DIR/target/site/apidocs
-webtgt=$jdb
+webtgt=$(mktemp -d)
+[ -z "$webtgt" ] && lgm "Error creating tmp dir, bailing." && exit 1
 mvn javadoc:javadoc
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-lgm "Cloning..."; 
-	git clone -q -b $jdb https://${gh_token}@github.com/$myid/$myproj $webtgt > /dev/null
-lgm "Copying..."; 
-	cd $webtgt; rm -rf apidocs && cp -Rf $websrc .
-lgm "Adding..."; 
-	git add -f . > /dev/null
-lgm "Committing..."; 
-	git commit -m "Published Javadoc: build $TRAVIS_BUILD_NUMBER" > /dev/null
-lgm "Pushing..."; 
-	git push -fq origin $jdb > /dev/null
-lgm "Done."
+lgm Cloning... 
+	git clone -b $jdb https://${gh_token}@github.com/$myid/$myproj $webtgt # > /dev/null
+	# git clone -qb $jdb https://${gh_token}@github.com/$myid/$myproj $webtgt > /dev/null
+lgm Copying...
+	rsync -r --delete $websrc $webtgt
+	# rsync -qr --delete $websrc $webtgt
+lgm Adding...
+	git add -f $webtgt # > /dev/null
+lgm Committing...
+	git commit -m "Published Javadoc: build $TRAVIS_BUILD_NUMBER" # > /dev/null
+lgm Pushing... 
+	git push -f origin $jdb # > /dev/null
+	# git push -fq origin $jdb > /dev/null
+lgm Done.
